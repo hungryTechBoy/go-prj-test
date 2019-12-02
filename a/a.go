da055fd4ec15ee408416fad203b31ce8bc8245b1
Bo Deng <bo.deng@shopee.com>
2019-12-02 18:26:42 +0800

    Add package a

---
+++ a/a.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/hungryTechBoy/go-prj-test/b"
+	"fmt"
+)
+
+const Name = b.Name
+
+func main() {
+	fmt.Println(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,3 @@
+module github.com/go-prj-test/submodules/a
+
+go 1.13
